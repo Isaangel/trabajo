cd45596b7272001185783fbd099ed47ddc4cfaf8
Isaangel <123569844+Isaangel@users.noreply.github.com>
2023-02-13 11:51:07 -0500

    seguimiento

--- app.js
+++ app.js
@@ -27,4 +27,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
     { name: "jirafa", img: "img/imagenes.jpg" }
   ];
+  const cuadricula = document.querySelector(".cuadricula");
+  const resultado = document.querySelector("#resultado");
+  var cartasEscogidas = [];
+  var cartasEscogidasId = [];
+  var cartasGanadas = [];
+  function crearTablero (){
+    for (let i = 0; i < cardAdj.length; i++) { 
+                                               
+      var carta = document.createElement('img'); 
+      carta.setAttribute('src', 'images/reverso.png');
+      carta.setAttribute('data-id', i); 
+      carta.addEventListener('click', voltearCarta);
+                                                    
+      cuadricula.appendChild(carta); 
+    }
+  }
+});
+
 });
